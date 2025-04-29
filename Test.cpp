a765994a10443e968d2b7e7b1a89f28a1878abb9
MMD-K <mohammadkalantari92@gmail.com>
2025-04-29 18:33:40 +0330

    ...

--- Test.cpp
+++ Test.cpp
@@ -6,9 +6,9 @@ const int MAX = 100;
 template<typename T,typename F>
 int myfind(T* begin,T* end,F f) {
     for (T* itr = begin; itr != end; itr++) {
-
-       ?
-
+        if (f(*itr) == 1) {
+            return itr - begin;
+        }
     }
     return -1;
 
@@ -61,9 +61,11 @@ public:
         cout << "Staff: " << username << ", Salary: " << salary << endl;
     }
     bool hasAccessToSee(AbstractUser* user) const override {
-        if (? != nullptr) {
+        if (dynamic_cast<Student *>(user) != nullptr) {
             return true;
         }
+
+        return false;
     }
 
 private:
@@ -80,49 +82,57 @@ public:
         cout << "Admin ";
         Staff::displayRole();
     }
-    ?
+    bool hasAccessToSee(AbstractUser* user) const override {
+        if (dynamic_cast<Admin *>(user) == nullptr) {
+            return true;
+        }
+        return false;
+    }
 };
 
-AbstractUser* current_user;
-AbstractUser* users[100];
-int user_cnt;
+AbstractUser* G_current_user;
+AbstractUser* G_users[100];
+int G_user_cnt;
 
 void print() {
-    if (!current_user) {
+    if (!G_current_user) {
         return;
     }
-    for (AbstractUser* user : users) {
-        if (?) {
+    for (AbstractUser* user : G_users) {
+        if (G_current_user->hasAccessToSee(user) == true) {
+            if (user == nullptr) {
+                return;
+            }
             user->displayRole();
         }
     }
 }
 
 void addStudent(string username, string password, int grade) {
-    int index = ?;
+    int index = myfind(G_users, G_users + G_user_cnt, [username](const AbstractUser *us){ if (username == us->getUsername()){return 1;} else {return 0;}});
     if (index != -1) {
         return;
     }
-    if (dynamic_cast<Admin*>(current_user) == nullptr){
+    if (dynamic_cast<Admin*>(G_current_user) == nullptr){
         cout << "permision denied"<<endl;
         return;
     }
     Student* st = new Student(username, password, grade);
-    users[user_cnt ++] = st;
+    G_users[G_user_cnt ++] = st;
 }
 
 int main() {
     Student s("erfan", "pass1", 12);
     Student s2("rasool", "pass2", 12);
     Staff st("alireza", "staffpass", 1000);
-    Staff st("kiyan", "staffpass", 1000);
+    Staff st2("kiyan", "staffpass", 1000);
     Admin a("yalda", "adminpass",1200);
-    user_cnt = 5;
-    users[0] = &s;
-    ?
-    ?
-    ?
-    ?
+    G_user_cnt = 5;
+    G_users[0] = &s;
+    G_users[1] = &s2;
+    G_users[2] = &st;
+    G_users[3] = &st2;
+    G_users[4] = &a;
 
     while (1) {
         string username, password;
@@ -130,12 +140,11 @@ int main() {
         cin >> username;
         cout << "password:";
         cin >> password;
-        int index = myfind(users, users + user_cnt, ?);
+        int index = myfind(G_users, G_users + G_user_cnt, [username](const AbstractUser *us){ if (username == us->getUsername()){return 1;} else {return 0;}});
         if (index!=-1) {
-            current_user = users[index];
+            G_current_user = G_users[index];
             break;
         }
-
     }
     int choice;
     while (1) {
@@ -150,7 +159,6 @@ int main() {
             int grade;
             cin >> user >> pass >> grade;
             addStudent(user, pass, grade);
-
         }
 
     }
